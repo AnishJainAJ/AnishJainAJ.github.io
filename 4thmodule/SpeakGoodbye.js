3ea364ee18894072d81811bdc588f206fe336788
AnishJainAJ <47848917+AnishJainAJ@users.noreply.github.com>
2020-05-11 17:35:42 +0530

    Delete SpeakGoodbye.js

--- 4thmodule/SpeakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function (global) {
-	var byeSpeaker = {};
-	var speakWord = "Good Bye";
-    byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-    }
-    global.byeSpeaker=byeSpeaker;
-})(window);
